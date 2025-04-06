5e967e16e23f89e1d19cf4a998e9f0068e99ad6f
Jesus Rodriguez <jesusrodaltamar@gmail.com>
2025-04-06 10:01:07 -0500

    Clases 6 y 7

---
+++ clase7_parametrosnombres.dart
@@ -0,0 +1,24 @@
+void main(){
+ 
+  print (greetEveryone());
+  print ("suma: ${addTwoNumbers(10,20)}");
+  print ("Suma opcional ${addTwoNumbersOptional (30,40)}");
+  print ("el mensaje es ${greetPerson (name:"francisco", message:"Saludando a")}");
+}
+
+greetEveryone() => "Hello everyone";
+
+//int addTwoNumbers (int a,int b){
+  //return a+b; 
+//}
+
+int addTwoNumbers (int a,int b) => a+b;
+
+int addTwoNumbersOptional (int a,[int b=0]){
+  //b = b ?? 0;
+  return a+b;
+}
+
+String greetPerson ({required String name, String message = "Hola"}){
+ return "$message, $name"; 
+}
